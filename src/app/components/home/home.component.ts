0b3038c0b27a4458ee43ba068806355aca569b21
SumittaBungsud <sumitta.hct@gmail.com>
2025-03-10 23:12:37 +0700

    created contact component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ProfileComponent } from './profile/profile.component';
 import { ProjectComponent } from '../project/project.component';
+import { ContactComponent } from '../contact/contact.component';
 
 @Component({
   selector: 'app-home',
-  imports: [ProfileComponent, ProjectComponent],
+  imports: [ProfileComponent, ProjectComponent, ContactComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.sass',
 })
