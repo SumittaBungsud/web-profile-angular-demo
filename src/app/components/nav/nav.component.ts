c75f21c195ee3ef10e3b48acb907b56e7d34ee8d
SumittaBungsud <sumitta.hct@gmail.com>
2025-04-18 21:46:34 +0700

    updated navbar as responsive

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-nav',
-  imports: [RouterLink],
+  imports: [RouterLink, FontAwesomeModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.sass',
 })
-export class NavComponent {}
+export class NavComponent {
+  win_middle_xs = window.innerWidth <= 840;
+  faEllipsisV = faEllipsisV;
+  isExpand = false;
+
+  onResize() {
+    this.win_middle_xs = window.innerWidth <= 840;
+  }
+}
